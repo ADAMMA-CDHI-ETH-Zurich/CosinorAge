abe5089ee9136461dfafa15425f3624ce2f920b8
jlohunecke <jacobhunecke@gmai.com>
2024-11-08 01:14:01 +0100

    adjust documentation

--- cosinorage/dataloaders/__init__.py
+++ cosinorage/dataloaders/__init__.py
@@ -11,3 +11,5 @@ from .dataloaders import AccelerometerDataLoader, ENMODataLoader, DataLoader
 from .utils.plot_enmo import plot_enmo, plot_enmo_difference
 from .utils.calc_enmo import calculate_enmo, calculate_minute_level_enmo
 from .utils.read_csv import read_acc_csvs, read_enmo_csv, filter_incomplete_days
+
+__all__ = ['AccelerometerDataLoader', 'ENMODataLoader', 'DataLoader']
--- setup.py
+++ setup.py
@@ -7,4 +7,13 @@ setup(
                 'accelerometer data.',
     packages=find_packages(),  # Automatically finds subpackages
     install_requires=[],  # Add any dependencies here
+
+    extras_require={
+        'docs': [
+            'sphinx',
+            'furo',
+            'pandas',
+            # other doc dependencies
+        ],
+    },
 )
