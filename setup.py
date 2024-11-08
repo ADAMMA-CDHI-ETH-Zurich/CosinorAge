7c7aac3b33c9fc895aa5d134116b83e9a14bce8d
jlohunecke <jacobhunecke@gmai.com>
2024-11-08 13:28:30 +0100

    removed cyclic refs

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='cosinorage',
-    version='0.1.2',
+    version='0.1.3',
     description='A package for computing the ConsinorAge from raw '
                 'accelerometer data.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jacob Leo Oskar Hunecke',
     url='https://github.com/jlohunecke/CosinorAge.git',
     packages=find_packages(),  # Automatically finds subpackages
