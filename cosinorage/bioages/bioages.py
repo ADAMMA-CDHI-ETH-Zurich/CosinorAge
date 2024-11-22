30adf0ccefb6f79afff2d80ece5baa0fc4b6522b
jlohunecke <jacobhunecke@gmai.com>
2024-11-22 17:18:32 +0100

    add plotting

--- cosinorage/bioages/bioages.py
+++ cosinorage/bioages/bioages.py
@@ -1,19 +1,31 @@
 from ..dataloaders import DataLoader
+from ..features.features import WearableFeatures
 
 # TODO: Implement Bioages class
-class Bioages:
-
-    def __init__(self, loader: DataLoader):
-
-        self.KDM_bioage = None
-        self.PhenoAge = None
-        self.CosinorAge = None
-
-    def compute_KDM_bioage(self):
-        pass
+class CosinorAge:
     
-    def compute_PhenoAge(self):
+    def __init__(self, features: WearableFeatures):
+        
+        # patient data
+        self.mesor = features.feature_df.loc["MESOR"]
+        self.amplitude = features.feature_df.loc["amplitude"]
+        self.acrophase = features.feature_df.loc["acrophase"]
+        self.chronological_age = None
+
+        # model parameters
+        self.fit = False
+
+        self.model = None
+        self.beta_1 = None
+        self.beta_2 = None
+        self.beta_3 = None
+        self.alpha = None
+
+    def fit(self):
         pass
 
-    def compute_CosinorAge(self):
-        pass
+    def predict(self):
+        adjustment = ((self.beta_1 * self.mesor) + (self.beta_2 * self.amplitude) + (self.beta_3 * self.acrophase))/self.alpha
+        cosinor_age = self.chronological_age - adjustment
+        return cosinor_age
+    
